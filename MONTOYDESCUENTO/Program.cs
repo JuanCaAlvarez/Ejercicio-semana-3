e8569352a323fd50e4986923c6372b4ab2ba5821
JUAN CARLOS ALVAREZ AYALA <jeici0507@gmail.com>
2025-10-29 00:01:13 -0500

    Agregar archivos de proyecto.

---
+++ MONTOYDESCUENTO/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//PASO 1: COLOCACION DE DATOS
+Console.WriteLine("Ingrese la cantidad de productos: ");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el costo de cada producto: ");
+decimal n2 = int.Parse(Console.ReadLine());
+
+//PASO 2: PROCESO
+decimal producto = n1 * n2;
+
+//PASO 3: EVALUAR EL DESCUENTO
+
+decimal Descuento = 0.0m;
+
+
+if (n1 > 20)
+{
+    Descuento = 0.10m;
+}
+else if (n1 >= 10)
+{
+    Descuento = 0.05m;
+}
+
+//PASO 4 : CALCULO DEL DESCUENTO
+decimal Montocondescuento = producto * Descuento;
+
+//PASO 5: CALCULO MONTO TOTAL
+decimal Montototal = producto - Montocondescuento;
+
+//PASO 6: RESULTADO
+Console.WriteLine($"Monto total: {Montototal}");
